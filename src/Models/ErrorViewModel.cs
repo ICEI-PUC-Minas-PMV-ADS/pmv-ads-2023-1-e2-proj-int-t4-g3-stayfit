de92e4473167a93da6584f7939e12c1b41c2e901
pauloosilas <paulo3356@gmail.com>
2023-03-18 17:25:50 -0300

    update diagrama de caso de uso

--- src/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StayFit.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
