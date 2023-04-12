86c09a560158b8e6979b5bf64ce7b19d411f7c00
pauloosilas <paulo3356@gmail.com>
2023-04-11 21:14:46 -0300

    Ferramentas e banco de dados criado

---
+++ src/StayFit/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StayFit.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
